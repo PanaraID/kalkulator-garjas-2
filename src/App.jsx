5d7be1e03c02d2e983030fe9ffc21ecfbba3572e
ahlaiptek <iptekahla@gmail.com>
2024-06-13 06:40:43 +0700

    Init bootstrap dan sedikit projek tentang ganti tema

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [theme, setTheme] = useState(null);
+  const resetTheme = () => {
+    setTheme(null);
+  };
+  const themeClass = theme ? theme.toLowerCase() : "secondary";
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Fragment>
+      <div
+        className={`text-capitalize h1 mb-4 w-100 text-center text-${themeClass}`}
+      >
+        {`${theme || "Default"} Theme`}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="btn-group">
+        <button className={`text-capitalize btn btn-${themeClass} btn-lg"`} type="button">
+          {theme ? theme + " theme" : "Choose Theme"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          type="button"
+          className={`btn btn-lg btn-${themeClass} dropdown-toggle dropdown-toggle-split`}
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <span className="visually-hidden">Toggle Dropdown</span>
+        </button>
+        <div className="dropdown-menu">
+          <a className="dropdown-item" onClick={() => setTheme("primary")}>
+            Primary Theme
+          </a>
+          <a className="dropdown-item" onClick={() => setTheme("danger")}>
+            Danger Theme
+          </a>
+          <a
+            className="dropdown-item" onClick={() => setTheme("success")}>
+            Success Theme
+          </a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#" onClick={() => resetTheme()}>
+            Default Theme
+          </a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Fragment>
+  );
 }
 
 export default App
