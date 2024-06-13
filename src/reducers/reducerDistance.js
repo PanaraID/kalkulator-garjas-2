d16518f46089205886b3025d4fc82d2ee2f674ea
ahlaiptek <iptekahla@gmail.com>
2024-06-13 11:13:07 +0700

    Kelupaan batas min dan max distance wanita

--- src/reducers/reducerDistance.js
+++ src/reducers/reducerDistance.js
@@ -2,16 +2,19 @@ import GENDERS from "../finals/GENDERS"
 import DISTANCE from "../finals/DISTANCE"
 
 const reducerDistance = (state, action) => {
-    const buffer = action.value
-    switch (action.type) {
-        case GENDERS.LELAKI:
-            if (buffer >= DISTANCE.LELAKI.MIN && buffer <= DISTANCE.LELAKI.MAX)
-                return {value: action.value}
-            else
-                return {value: -1}
-        case GENDERS.PEREMPUAN:
-            return {value: action.value}
-    }
+  const buffer = action.value
+  switch (action.type) {
+    case GENDERS.LELAKI:
+      if (buffer >= DISTANCE.LELAKI.MIN && buffer <= DISTANCE.LELAKI.MAX)
+        return { value: action.value }
+      else
+        return { value: -1 }
+    case GENDERS.PEREMPUAN:
+      if (buffer >= 22 && buffer <= 3095)
+        return { value: action.value }
+      else
+        return { value: -1 }
+  }
 }
 
 export default reducerDistance
