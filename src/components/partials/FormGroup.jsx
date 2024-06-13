92190a4840bda97371e418399fabe4f2025e78ed
ahlaiptek <iptekahla@gmail.com>
2024-06-13 11:51:39 +0700

    Menambahkan prop types

--- src/components/partials/FormGroup.jsx
+++ src/components/partials/FormGroup.jsx
@@ -1,4 +1,5 @@
-import { useId, useState, useEffect } from "react"
+import { useId, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 const FormGroup = ({ data, gender, min, max, placeholder, onChange }) => {
   const id = useId()
@@ -11,7 +12,7 @@ const FormGroup = ({ data, gender, min, max, placeholder, onChange }) => {
   return (
     <div className='form-group card p-2'>
       <label htmlFor={id}>{placeholder}</label>
-      <input type='number' className='form-control' id={id} placeholder={placeholder} onChange={onChange} />
+      <input type='number' className='form-control' id={id} placeholder={data.value} onChange={onChange} />
       {help && (
         <div className='alert alert-danger' role='alert'>
           Jarak untuk {gender} minimalnya adalah {min} dan maksimalnya adalah {max}
@@ -24,6 +25,15 @@ const FormGroup = ({ data, gender, min, max, placeholder, onChange }) => {
       )}
     </div>
   )
+}
+
+FormGroup.propTypes = {
+  data: PropTypes.object.isRequired,
+  gender: PropTypes.string.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default FormGroup
