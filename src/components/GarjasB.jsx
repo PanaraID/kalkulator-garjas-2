ccfce2dbe78f069178b18d45f70a8f2f61a27e53
ahlaiptek <iptekahla@gmail.com>
2024-06-13 11:58:48 +0700

    Menulis test semua garjas

--- src/components/GarjasB.jsx
+++ src/components/GarjasB.jsx
@@ -1,5 +1,36 @@
-const GarjasB = () => {
+import { useContext } from 'react'
 
+import AppContext from '../context/AppContext'
+import FormGroup from './partials/FormGroup'
+
+import DISTANCE from '../finals/DISTANCE'
+
+const GarjasB = () => {
+  const { gender, distance, dispatchDistance } = useContext(AppContext)
+
+  const onChange = e => {
+    dispatchDistance({ type: gender, value: e.target.value })
+  }
+
+  return (
+    <section className='garjas'>
+      <h3>Garjas B</h3>
+      <FormGroup
+        data={distance}
+        gender={gender}
+        min={DISTANCE[gender.toUpperCase()].MIN}
+        max={DISTANCE[gender.toUpperCase()].MAX}
+        placeholder={"Masukkan jarak"}
+        onChange={onChange} />
+      <FormGroup
+        data={distance}
+        gender={gender}
+        min={DISTANCE[gender.toUpperCase()].MIN}
+        max={DISTANCE[gender.toUpperCase()].MAX}
+        placeholder={"Masukkan jarak"}
+        onChange={onChange} />
+    </section>
+  )
 }
 
 export default GarjasB
